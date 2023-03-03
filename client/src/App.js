f68d554fd19cfbdb3c3e5a6e2d806d4c55b7144c
Nick Quirk <n.quirk87@gmail.com>
2023-03-03 14:32:43 +0000

    added interface components

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 // React Imports 
 import { useEffect, useState } from 'react'
 
+// Bootstrap Imports 
+import { Container, Row, Col, Form, Button, ListGroup, ListGroupItem } from 'react-bootstrap'
+
 // Imports
 import axios from 'axios'
 
@@ -30,9 +33,50 @@ const App = () => {
     getData()
   }, [])
 
-  return ( 
-    <h1>Product Search</h1>
-    
+  const handleChange = (e) => {
+    console.log(e.target.value)
+    setLocation(e.target.value)
+  }
+
+
+  return (
+    <Container>
+      <Row>
+        <h1>Product Search</h1>
+      </Row>
+      <Row>
+        <Form>
+          <Row>
+            <Form.Group as={Row} className='search-form'>
+              <Col sm={2}>
+                <Form.Label>
+                    Title
+                </Form.Label>
+              </Col>
+              <Col>
+                <Form.Control type='text' onChange={handleChange}/>
+              </Col>
+            </Form.Group>
+            <Col>
+              <Button type='submit'>Submit</Button>
+            </Col>
+          </Row>
+        </Form>
+      </Row>
+      <ListGroup className='product-list'>
+        <ListGroupItem className='list-group'>
+          <div>
+            <img className='product-image' src='https://res.cloudinary.com/dhjguxvm1/image/upload/v1669369462/sample.jpg' ></img>
+          </div>
+          <div className='product-title'>
+            <h4>Title</h4>
+          </div>
+          <div className='product-destination'>
+            <h4>Destination</h4>
+          </div>
+        </ListGroupItem>
+      </ListGroup>
+    </Container> 
   ) 
 }
 
