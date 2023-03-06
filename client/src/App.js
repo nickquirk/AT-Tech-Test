7dfacf67e932d83ac3426eca97172ed11e37006b
Nick Quirk <n.quirk87@gmail.com>
2023-03-06 12:23:08 +0000

    populated table with API data

--- client/src/App.js
+++ client/src/App.js
@@ -10,11 +10,7 @@ import axios from 'axios'
 // TODO
 // - use offset for pagination 
 //    - limit parameter to change number of reults per page 
-// - error handling 
-// - try/catch blocks 
-//    - 404
 // - spinner for loading 
-// - Titles above list 
 // - Remake with table rather than List group 
 
 // ? Styling
@@ -36,7 +32,7 @@ const App = () => {
   useEffect(() => {
     const getData = async () => {
       try {
-        const { data } = await axios.get('https://global.atdtravel.com/api/products?geo=en') 
+        const { data } = await axios.get('https://global1.atdtravel.com/api/products?geo=en')
         console.log(data)
         setProducts(data)
         setErrors(false) // clear error state
@@ -56,7 +52,7 @@ const App = () => {
   // call API with user search results 
   const getSearchData = async () => {
     try {
-      const { data } = await axios.get(`https://global.atdtravel.com/api/products?geo=${formData.region}&title=${formData.title}`) 
+      const { data } = await axios.get(`https://global1.atdtravel.com/api/products?geo=${formData.region}&title=${formData.title}`)
       console.log(data)
       setProducts(data)
       setErrors(false) // clear error state
@@ -98,67 +94,36 @@ const App = () => {
             <Form.Group as={Row} className='search-form'>
               <Col sm={2} className='form-title'>
                 <Form.Label>
-                    Title
+                  Title
                 </Form.Label>
               </Col>
               <Col>
-                <Form.Control name='title' type='text' value={formData.title} onChange={handleChange}/>
+                <Form.Control name='title' type='text' value={formData.title} onChange={handleChange} />
               </Col>
             </Form.Group>
-            <Col>
+            <Col className='d-flex align-items-center'>
               <Button type='submit'>Submit</Button>
             </Col>
-            <Col>
+            <Col className='d-flex align-items-center'>
+              <Form.Label>
+                Region
+              </Form.Label>
               <Form.Group>
-                <Form.Label>
-                    Region
-                </Form.Label>
-                <Col>
-                  <Form.Select 
-                    name='region' 
-                    className='region-select' 
-                    value={formData.region} 
-                    onChange={handleRegionChange}>
-                    <option value='en'>UK</option>
-                    <option value='en-ie'>Ireland</option>
-                    <option value='de-de'>Germany</option>
-                  </Form.Select>
-                </Col>
+                <Form.Select
+                  name='region'
+                  className='region-select'
+                  value={formData.region}
+                  onChange={handleRegionChange}>
+                  <option value='en'>UK</option>
+                  <option value='en-ie'>Ireland</option>
+                  <option value='de-de'>Germany</option>
+                </Form.Select>
               </Form.Group>
             </Col>
           </Row>
         </Form>
       </Row>
-      <ListGroup className='product-list'>
-        { products.data ? 
-          products.data.map(product => {
-            const { id, title, dest } = product
-            return (
-              <ListGroupItem key={id} className='list-group'>
-                <div>
-                  <img className='product-image' src={product.img_sml}></img>
-                </div>
-                <div className='product-title'>
-                  <p>{title}</p>
-                </div>
-                <div className='product-destination'>
-                  <p>{dest}</p>
-                </div>
-              </ListGroupItem>
-            )
-          })
-          : errors ?
-            <div className='messages'>
-              <p>Oops, looks like there&apos;s an error...</p>
-              <p className='error'>{errorMessage}</p>
-            </div>
-            : 
-            <div className='messages'>
-              <p>Loading...</p>
-            </div>
-        }
-      </ListGroup>
-      <Table striped border hover>
+      <Table striped hover>
         <thead>
           <tr>
             <th>Image</th>
@@ -167,20 +132,32 @@ const App = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td><img className='product-image' src='https://res.cloudinary.com/dhjguxvm1/image/upload/v1669369462/sample.jpg'/></td>
-            <td>I&apos;m baby crucifix cloud bread heirloom jianbing fit selvage affogato chia gatekeep. Cray prism listicle fashion axe, </td>
-            <td>London</td>
-          </tr>
-          <tr>
-            <td><img className='product-image' src='https://res.cloudinary.com/dhjguxvm1/image/upload/v1669369462/sample.jpg'/></td>
-            <td>Title here....</td>
-            <td>London</td>
-          </tr>
+          {products.data ?
+            products.data.map(product => {
+              const { id, title, dest } = product
+              return (
+                <tr key={id}>
+                  <td><img className='product-image' src={product.img_sml} /></td>
+                  <td>{title}</td>
+                  <td>{dest}</td>
+                </tr>
+              )
+            })
+            : errors ?
+              <div className='messages'>
+                <p>Oops, looks like there&apos;s an error...</p>
+                <p className='error'>{errorMessage}</p>
+              </div>
+              :
+              <div className='messages'>
+                <p>Loading...</p>
+              </div>
+          }
+
         </tbody>
       </Table>
-    </Container> 
-  ) 
+    </Container>
+  )
 }
 
 export default App
